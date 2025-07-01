def0424fddeaff702c155f027b50983a46ce8f94
devGO <devbin20@gmail.com>
2025-07-01 21:27:11 +0900

    [Solve] ✍️ 1890.점프 수정

--- Week15/1890_eb.js
+++ Week15/1890_eb.js
@@ -5,16 +5,16 @@ const N = Number(input[0])
 const board = input.slice(1).map(line => line.split(' ').map(Number))
 const dp = Array.from({ length: N }, () => Array(N).fill(BigInt(0)))
 
-dp[0][0] = 1 // 시작점 도달 가능한 경로 수는 1
+dp[0][0] = BigInt(1) // 시작점 도달 가능한 경로 수는 1
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
     if (dp[i][j] === 0 || board[i][j] === 0) continue
     const move = board[i][j]
     if (j + move < N) {
-      dp[i][j + move] += BigInt(dp[i][j])
+      dp[i][j + move] += dp[i][j]
     }
     if (i + move < N) {
-      dp[i + move][j] += BigInt(dp[i][j])
+      dp[i + move][j] += dp[i][j]
     }
   }
 }
